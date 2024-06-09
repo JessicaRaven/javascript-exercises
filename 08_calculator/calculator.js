37708dc355f9e9d12d144bc0330d94eae6737528
JessicaRaven <129120706+JessicaRaven@users.noreply.github.com>
2024-06-08 23:27:57 -0400

    completed exercise 8&9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((sum,number)=>sum+=number,0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  return nums.reduce((product,number)=>product*=number,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number==0) return 1;
+  let product = 1;
+  for(let i=number;i>0;i--){
+    product*=i;
+  }
+	return product;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
-};
+const palindromes = function (...words) {
+    words=words.join('');
+    words=words.toLowerCase().split('').filter((char)=> /([a-z]|[A-Z]|[0-9])/.test(char));
+    let reflection = [];
+    for(let i=0; i<words.length; i++){
+        reflection[i]=words[i];
+    }
+    console.log('words:'+words);
+    console.log('flip'+reflection);
+    reflection.reverse();
+    console.log('words2:'+words);
+    console.log('flip2'+reflection);
+    reflection=reflection.join('');
+    words=words.join('');
+    //console.log("string: "+ words);
+    //console.log("reflection: " + reflection);
+    return reflection===words;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
