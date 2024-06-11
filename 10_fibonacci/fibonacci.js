da521316e6938c9f5c810a7b10162b19dda821fc
JessicaRaven <129120706+JessicaRaven@users.noreply.github.com>
2024-06-10 22:07:58 -0400

    completed exercises 10-12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+//take in input number and create fibonacci
+    num = +num;
+    let sequence=[];
+    sequence[0]=1;
+    sequence[1]=1;
+    for(let i=2; i<num; i++){
+        sequence[i]=sequence[i-1] + sequence [i-2];
+    }
+    if(num>0) return sequence[num-1];
+    else return 'OOPS';
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let date= new Date();
+    let year=date.getFullYear();
+    people.sort((a,b)=>{
+        if('yearOfDeath' in a && 'yearOfDeath' in b){
+            return (b.yearOfDeath-b.yearOfBirth)-(a.yearOfDeath-a.yearOfBirth);
+        }
+        else if (! 'yearOfDeath' in a && 'yearOfDeath' in b){
+            //let date = date();
+            //let year=date.getFullYear();
+            return ((b.yearOfDeath-b.yearOfBirth)-year);
+        }
+        else if ('yearOfDeath' in a && ! 'yearOfDeath' in b){
+            //let date = date();
+            //let year=date.getFullYear();
+            return (year-(a.yearOfDeath-a.yearOfBirth));
+        }
+        else{
+            //let date = date();
+            //let year=date.getFullYear();
+            return ((year-b.yearOfBirth)-(year-a.yearOfBirth));
+        }
+    }
+    )
+    return people[0];
 };
 
 // Do not edit below this line
